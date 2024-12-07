2e59d964bd8965ce6888bf85a9539c3068bdd2bb
sgerhardt <sean_gerhardt@hotmail.com>
2024-12-07 08:35:11 -0600

    day 5 part 2

--- src/day5/rules.test.ts
+++ src/day5/rules.test.ts
@@ -1,6 +1,6 @@
 import {describe, it} from "node:test";
 import {strict as assert} from "assert";
-import {isOrderingCorrect, loadRulesForNumbers} from "./rules";
+import {isOrderingCorrect, loadRulesForNumbers, order} from "./rules";
 
 
 describe('Part 1', () => {
@@ -15,3 +15,13 @@ describe('Part 1', () => {
         assert.equal(isOrderingCorrect(update), false);
     });
 });
+
+describe('Part 2', () => {
+    loadRulesForNumbers('src/day5/test_data.txt');
+    it('orders incorrect update', () => {
+        const update = [75, 97, 47, 61, 53];
+        // assert the two arrays are equal
+
+        assert.deepEqual(order(update), [97, 75, 47, 61, 53]);
+    });
+})
--- src/day5/rules.ts
+++ src/day5/rules.ts
@@ -5,38 +5,79 @@ export function isOrderingCorrect(update: number[]): boolean {
     for (let i = 0; i < update.length; i++) {
         let num = update[i];
         let rules = numberToRules.get(num);
-        if (rules) {
-            for (let rule of rules) {
-                // e.g. update (75,47,61,53,29) is in the right order:
-                // 75 is correctly first because there are rules that put each other page after it: 75|47, 75|61, 75|53, and 75|29.
-                // now check each value in the update array to see if the rule is violated
-                if (num === rule.first) {
-                    // num must come before the other value in the rule
-                    let other = rule.second;
-                    for (let j = i + 1; j < update.length; j++) {
-                        if (update[j] === other) {
-                            if (j < i) {
-                                return false
-                            }
-                            break;
+        if (!rules) {
+            continue
+        }
+        for (let rule of rules) {
+            // e.g. update (75,47,61,53,29) is in the right order:
+            // 75 is correctly first because there are rules that put each other page after it: 75|47, 75|61, 75|53, and 75|29.
+            // now check each value in the update array to see if the rule is violated
+            if (num === rule.first) {
+                // num must come before the other value in the rule
+                let other = rule.second;
+                for (let j = i + 1; j < update.length; j++) {
+                    if (update[j] === other) {
+                        if (j < i) {
+                            return false
                         }
+                        break;
                     }
-                } else {
-                    // num must come after the other value in the rule
-                    let other = rule.first;
-                    for (let j = i + 1; j < update.length; j++) {
-                        if (update[j] === other) {
-                            if (j > i) {
-                                return false
-                            }
-                            break;
+                }
+            } else {
+                // num must come after the other value in the rule
+                let other = rule.first;
+                for (let j = i + 1; j < update.length; j++) {
+                    if (update[j] === other) {
+                        if (j > i) {
+                            return false
                         }
+                        break;
                     }
                 }
             }
         }
     }
     return true
+}
+
+export function order(update: number[]): number[] {
+    // order the update according to the rules
+    for (let i = 0; i < update.length; i++) {
+        let rules = numberToRules.get(update[i]);
+        if (!rules) {
+            continue
+        }
+        for (let rule of rules) {
+            // if any of the rules have a number that appears before the current update[i], we need to move it before it
+            if (rule.first === update[i]) {
+                let other = rule.second;
+                for (let j = i + 1; j < update.length; j++) {
+                    if (update[j] === other) {
+                        // move the other number to the current index
+                        let temp = update[j];
+                        update[j] = update[i];
+                        update[i] = temp;
+                        break;
+                    }
+                }
+            }
+            if (rule.second === update[i]) {
+                let other = rule.first;
+                for (let j = i + 1; j < update.length; j++) {
+                    if (update[j] === other) {
+                        // move the other number to the current index
+                        let temp = update[j];
+                        update[j] = update[i];
+                        update[i] = temp;
+                        break;
+                    }
+                }
+            }
+
+        }
+
+    }
+    return update;
 }
 
 type rule = {
@@ -86,4 +127,29 @@ export function part1() {
     }
 }
 
-part1();
+
+function part2() {
+    const updatesInput = fs.readFileSync('src/day5/updates_input.txt', 'utf8');
+    loadRulesForNumbers('src/day5/rules_input.txt')
+    const updateLines = updatesInput.split('\n');
+    let sum = 0
+    for (let updateLine of updateLines) {
+        const updatesStr = updateLine.split(",");
+
+        let updates: number[] = [];
+        updatesStr.map((str) => updates.push(Number(str)));
+
+        if (isOrderingCorrect(updates)) {
+            continue
+        }
+        while (!isOrderingCorrect(updates)) {
+            updates = order(updates);
+        }
+        // add the middle index
+        sum += updates[Math.floor(updates.length / 2)]
+        console.log(sum);
+    }
+}
+
+// part1();
+part2();
